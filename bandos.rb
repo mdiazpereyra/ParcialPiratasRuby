d0df7f1c69d001eb55eb5e5d5c2646e32092fa1d
Mariano Díaz <37786555+mdiazpereyra@users.noreply.github.com>
2018-11-24 21:03:12 -0300

    Subidos Barco y bandos

---
+++ bandos.rb
@@ -0,0 +1,17 @@
+module armadaInglesa
+	def bonus(barco)
+		barco.recibirBonusDeArmadaInglesa
+	end
+end
+
+module unionPirata
+	def bonus(barco)
+		barco.recibirBonusDeUnionPirata
+	end
+end
+
+module armadaDelHolandesErrante
+	def bonus(barco)
+		barco.recibirBonusDeArmadaDelHolandesErrante
+	end
+end
